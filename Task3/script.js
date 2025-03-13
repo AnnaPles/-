ed25da93ad24b91df12c4a9c302c768620451ed1
AnnaPles <Merloanna@mail.ru>
2025-03-13 14:39:18 +0500

    Update script.js

--- Task3/script.js
+++ Task3/script.js
@@ -2,16 +2,14 @@ let currentIndex = 0;
 const slides = document.querySelectorAll('.slide');
 const captionText = document.getElementById('caption-text');
 
-document.getElementById('next').addEventListener('click', ()
-=> {
+document.getElementById('next').addEventListener('click', () => {
   slides[currentIndex].classList.remove('active');
   currentIndex = (currentIndex + 1) % slides.length;
   slides[currentIndex].classList.add('active');
   updateCaption();
 });
 
-document.getElementById('prev').addEventListener('click', ()
-=> {
+document.getElementById('prev').addEventListener('click', () => {
   slides[currentIndex].classList.remove('active');
   currentIndex = (currentIndex - 1 + slides.length) %
   slides.length;
